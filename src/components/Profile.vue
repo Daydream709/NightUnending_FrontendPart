b6d5d11afe4bc2d93f784d89309d486635faf2cb
Daydream <Daydream709@outlook.com>
2025-08-28 11:28:22 +0800

    除了两处图标，全部完成

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,7 +1,37 @@
 <template>
     <div class="page">
-        <h1>我的</h1>
-        <p>这是个人中心页面内容</p>
+        <div class="maininfo">
+            <div class="avatar"></div>
+            <div class="nameiconid">
+                <div class="nameicon">
+                    <div class="name">XXX</div>
+                    <div class="icon"><img class="down" src="../../public/source/icon/sleepsituation.png" alt=""></div>
+                </div>
+                <div class="id">id : 111111111</div>
+            </div>
+        </div>
+        <div class="options">
+            <div class="sleepsituation">
+                <img class="opticon" src="../../public/source/icon/sleepsituation.png" alt="">
+                <span class="content">睡眠情况</span>
+                <span class="arrowhead">></span>
+            </div>
+            <div class="healthanalytics">
+                <img class="opticon" src="../../public/source/icon/healthanalytics.png" alt="">
+                <span class="content">健康分析</span>
+                <span class="arrowhead">></span>
+            </div>
+            <div class="accomplishment">
+                <img class="opticon" src="../../public/source/icon/sleepsituation.png" alt="">
+                <span class="content">成就</span>
+                <span class="arrowhead">></span>
+            </div>
+            <div class="settings">
+                <img class="opticon" src="../../public/source/icon/settings.png" alt="">
+                <span class="content">设置</span>
+                <span class="arrowhead">></span>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -14,7 +44,84 @@
     text-align: center;
 }
 
-h1 {
-    color: #42b983;
+.maininfo {
+    height: 26vh;
+    position: relative;
+}
+
+.avatar {
+    width: 24.26vw;
+    height: 24.26vw;
+    border-radius: 12.13vw;
+    background-color: #D9D9D9;
+    position: absolute;
+    top: 9.6vh;
+    left: 7.2vw;
+}
+
+.nameiconid {
+    display: inline-block;
+    position: absolute;
+    top: 11.5vh;
+    left: 34.1vw;
+    text-align: left;
+    line-height: 1.4;
+}
+
+.name {
+    display: inline-block;
+    font-size: 6.4vw;
+}
+
+.icon {
+    display: inline-block;
+}
+
+.id {
+    font-size: 3.2vw;
+}
+
+
+
+.sleepsituation,
+.healthanalytics,
+.accomplishment,
+.settings {
+    height: 5.54vh;
+    background-color: #EBEBEB;
+    text-align: left;
+    line-height: 5.54vh;
+    font-size: 4vw;
+    color: #454545;
+
+}
+
+.sleepsituation {
+    margin-bottom: 0.4vh;
+}
+
+.healthanalytics,
+.accomplishment {
+    margin-bottom: 3.5vh;
+}
+
+.opticon {
+    width: 7vw;
+    height: auto;
+    vertical-align: middle;
+    margin: 0 2vw;
+}
+
+.arrowhead {
+    float: right;
+    margin-right: 4vw;
+    color: #454545;
+    font-size: 5vw;
+}
+
+.down {
+    width: 6vw;
+    height: auto;
+    vertical-align: sub;
 }
 </style>
